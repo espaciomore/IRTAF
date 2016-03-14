e60c32ad094a831caf74e43c99219d5838f2d9f8
Manuel A. Cerda <espaciomore@gmail.com>
2016-03-14 19:40:58 -0400

    Update IRFindsBy.cs

--- IRFindsBy.cs
+++ IRFindsBy.cs
@@ -13,12 +13,25 @@ namespace FASTSelenium.ImageRecognition
         public int Right { get; set; }
         public int Top { get; set; }
         public int Bottom { get; set; }
-        public int Offset_X { get; set; }
-        public int Offset_Y { get; set; }
         public string Text { get; set; }
         public Dictionary<string, string> Attributes { get; set; }
         public Dictionary<string, string> CssValues { get; set; }
 
+
+        protected int Offset_X { get; set; }
+        protected int Offset_Y { get; set; }
+
         public IRFindsBy() : base() { }
+
+        public void SetOffset(int dx, int dy)
+        {
+            this.Offset_X = dx;
+            this.Offset_Y = dy;
+        }
+
+        public System.Windows.Point GetOffset()
+        {
+            return new System.Windows.Point(this.Offset_X, this.Offset_Y);
+        }
     }
 }
