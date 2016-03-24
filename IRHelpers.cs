43a760269e20a41b8dfd1b5309184c958ae92522
Manuel A. Cerda <espaciomore@gmail.com>
2016-03-24 19:46:59 -0400

    Update IRHelpers.cs

--- IRHelpers.cs
+++ IRHelpers.cs
@@ -1,37 +1,76 @@
 using System;
-using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace FASTSelenium.ImageRecognition
 {
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
-    public class IRFindsBy : System.Attribute
+    public static class IRHelpers
     {
-        public readonly int Left = 0;
-        public readonly int Top = 0;
-
-        public string URI { get; set; }
-        public RotateFlipType RotateOrFlip { get; set; }
-        public int Right { get; set; }
-        public int Bottom { get; set; }
-        public string Text { get; set; }
-        public Dictionary<string, string> Attributes { get; set; }
-        public Dictionary<string, string> CssValues { get; set; }
-
-        protected int Offset_X { get; set; }
-        protected int Offset_Y { get; set; }
-
-        public IRFindsBy() : base() { }
-
-        public void SetOffset(int dx, int dy)
+        public static void InitElements<T>(T pageObject)
         {
-            this.Offset_X = dx;
-            this.Offset_Y = dy;
+            foreach (PropertyInfo property in pageObject.GetType().GetProperties())
+            {
+                if (property.PropertyType == typeof(IRButton))
+                    property.SetValue(pageObject, new IRButton(GetAttributeFrom<IRFindsBy>(pageObject, property.Name)));                
+            }
         }
 
-        public System.Windows.Point GetOffset()
+        public static T GetAttributeFrom<T>(this object instance, string propertyName) where T : Attribute
         {
-            return new System.Windows.Point(this.Offset_X, this.Offset_Y);
+            var attrType = typeof(T);
+            var property = instance.GetType().GetProperty(propertyName);
+
+            return (T)property.GetCustomAttributes(attrType, false).First();
+        }
+
+        public static void RetryUntil(IRButton element, TimeSpan timeout)
+        {
+            try
+            {
+                var _startedTime = DateTime.Now.TimeOfDay.TotalMilliseconds - 1;
+                var _waitTime = timeout.TotalMilliseconds;
+                var findElementException = "None";
+                while ((DateTime.Now.TimeOfDay.TotalMilliseconds - _startedTime) < _waitTime)
+                {
+                    try
+                    {
+                        element.FindElement();
+                        return;
+                    }
+                    catch (Exception ex)
+                    {
+                        findElementException = ex.Message;
+                    }
+                };
+                
+                throw new Exception(string.Format("Time Interval of {0} second(s) with Exception: {1}", System.Convert.ToString(timeout.TotalSeconds), findElementException));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("IRHelpers.RetryUntil timed out: " + ex.Message);
+            }
+        }
+
+        public static void WaitUntil(TimeSpan timeout)
+        {            
+            var _startedTime = DateTime.Now.TimeOfDay.TotalMilliseconds - 1;
+            var _waitTime = timeout.TotalMilliseconds;
+            while ((DateTime.Now.TimeOfDay.TotalMilliseconds - _startedTime) < _waitTime) { };
+        }
+
+        public static void SaveInReportDir(Bitmap b)
+        {
+            using (var tmpImage = new Bitmap(b))
+            {
+                if (!Directory.Exists(IRConfig.OutputPath.TrimEnd('\\')))
+                    Directory.CreateDirectory(IRConfig.OutputPath.TrimEnd('\\'));
+
+                var filename = "ScreenSample_" + DateTime.UtcNow.ToString("ddMMMyyyy_HHmmss_fffffff") + ".BMP";
+                tmpImage.Save(Path.Combine(IRConfig.OutputPath, filename), ImageFormat.Bmp);
+            }
         }
     }
 }
