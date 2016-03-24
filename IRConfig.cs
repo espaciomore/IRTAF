4f671147fdfc6de32c4c0fd00ff2bd766c9b3945
Manuel A. Cerda <espaciomore@gmail.com>
2016-03-24 19:44:55 -0400

    Update IRConfig.cs

--- IRConfig.cs
+++ IRConfig.cs
@@ -6,6 +6,8 @@ namespace FASTSelenium.ImageRecognition
         public static bool canSaveScreenSamples = false;
 
         public static int waitTime = 60;
+        
+        public static readonly System.Windows.Size screenSize = new System.Windows.Size(1920, 1080);
 
         public static string MediaPath { get; set; }
 
