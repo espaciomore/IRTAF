8a28373f74a22e179be56b7e72c53697d3f85722
Manuel A. Cerda <espaciomore@gmail.com>
2016-03-30 19:51:10 -0400

    Update IRFindsBy.cs

--- IRFindsBy.cs
+++ IRFindsBy.cs
@@ -15,24 +15,87 @@ namespace FASTSelenium.ImageRecognition
         public int Right { get; set; }
         public int Bottom { get; set; }
         public string Text { get; set; }
-        public Dictionary<string, string> Attributes { get; set; }
-        public Dictionary<string, string> CssValues { get; set; }
 
-
-        protected int Offset_X { get; set; }
-        protected int Offset_Y { get; set; }
+        public Dictionary<string, string> Attributes = new Dictionary<string, string>();
+        public Dictionary<string, string> CssValues = new Dictionary<string, string>();
+        public Dictionary<string, System.Windows.Point> OffsetValues = new Dictionary<string, System.Windows.Point>();
+        
 
         public IRFindsBy() : base() { }
 
-        public void SetOffset(int dx, int dy)
+
+        protected int GetOffset_X()
         {
-            this.Offset_X = dx;
-            this.Offset_Y = dy;
+            return GetOffset_X(this.GetOffsetKey());
+        }
+
+        private int GetOffset_X(string OffsetKey)
+        {
+            if (!OffsetValues.ContainsKey(OffsetKey))
+                throw new Exception("IRFindsBy.Offset_X does not exist for " + OffsetKey);
+
+            return System.Convert.ToInt32(OffsetValues[OffsetKey].X);
+        }
+
+        protected int GetOffset_X(int dX, int dY)
+        {
+            var OffsetKey = this.GetOffsetKey(dX, dY);
+
+            return GetOffset_X(OffsetKey);
+        }
+
+        protected int GetOffset_Y()
+        {
+            return GetOffset_Y(this.GetOffsetKey());
+        }
+
+        private int GetOffset_Y(string OffsetKey)
+        {
+            if (!OffsetValues.ContainsKey(OffsetKey))
+                throw new Exception("IRFindsBy.Offset_Y does not exist for " + OffsetKey);
+
+            return System.Convert.ToInt32(OffsetValues[OffsetKey].Y);
+        }
+
+        protected int GetOffset_Y(int dX, int dY)
+        {
+            var OffsetKey = this.GetOffsetKey(dX, dY);
+
+            return GetOffset_Y(OffsetKey);
+        }
+
+        public void SetOffset(int dx, int dy, int dX, int dY)
+        {
+            var offsetKey = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0:#}x{1:#}", dX, dY);
+            OffsetValues.Add(offsetKey, new System.Windows.Point(dx, dy));
         }
 
         public System.Windows.Point GetOffset()
         {
-            return new System.Windows.Point(this.Offset_X, this.Offset_Y);
+            return new System.Windows.Point(this.GetOffset_X(), this.GetOffset_Y());
+        }
+
+        private System.Windows.Size GetScreenSize()
+        {
+            return System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size.Convert();
+        }
+
+        private string GetOffsetKey(int? dX=null, int? dY=null)
+        {
+            var screenSize = GetScreenSize();
+            var OffsetKey = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0:#}x{1:#}", dX ?? 0, dY ?? 0);
+
+            if (!this.OffsetValues.ContainsKey(OffsetKey))
+            {
+                OffsetKey = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0:#}x{1:#}", screenSize.Width, screenSize.Height);
+            }
+
+            if (!this.OffsetValues.ContainsKey(OffsetKey))
+            {
+                OffsetKey = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0:#}x{1:#}", IRConfig.screenSize.Width, IRConfig.screenSize.Height);
+            }
+
+            return OffsetKey;
         }
     }
 }
